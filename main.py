866fa14317c94951994cd43892e96ae65fc7b50b
Kamyshin Ivan <ikamyshin@yandex.ru>
2022-04-01 20:55:56 +0300

    task hello-world ready

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import telebot
+import json
+
+bot = telebot.TeleBot('5138350103:AAFmt04RrYlet0aGGDgpfYrTV1DbBPNQTrA')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	help_commands = 'Привет, я бот и помогу тебе подобрать лучший отель\n' \
+					'\nПодсказка по моим командам:\n' \
+					'\n/help - команды бота\n' \
+					'/lowprice - самые дешёвые отели в городе\n' \
+					'/highprice - самые дорогие отели в городе\n' \
+					'/bestdeal - отели, наиболее подходящие по цене и расположению от центра\n' \
+					'/history - вывод истории поиска отелей\n'
+	bot.send_message(message.from_user.id, help_commands)
+
+
+@bot.message_handler(commands=['hello-world'])
+def send_welcome(message):
+	bot.send_message(message.from_user.id, 'Привет')
+
+
+@bot.message_handler(content_types=['text'])
+def send_welcome(message):
+	if message.text.lower() == 'привет':
+		bot.send_message(message.from_user.id, 'Привет')
+	else:
+		echo_all(message)
+
+
+@bot.message_handler(content_types=['text'])
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
